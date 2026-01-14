45bece9cc9665f17be2b4da2c045d2ee204c512f
nguyenhuuloc357 <locn0920@gmail.com>
2026-01-14 12:38:25 +0700

    test mới
    mệt mỏi

--- sapthanhcong.c
+++ sapthanhcong.c
@@ -1,15 +1,25 @@
 #include<stdio.h>
 #include<math.h>
+int ngto(int n){
+	for(int i=2;i<=sqrt(n);i++){
+	if(n%i==0)
+	return 0;
+	}
+	return n>1;
+}
 int main(){
 	int n;
 	scanf("%d",&n);
-	long long tong=0;
-	for(int i=1;i<=n;i++){
-//chẵn là 2,4,6,8 lẻ 1,3,5,7
-	tong+=2*i;
-}
-	printf("%lld",tong);
-return 0;
-}
-
-	
+	int a[n];
+	for(int i=0;i<n;i++){
+	scanf("%d",&a[i]);}
+	int cnt=0,sum=0;
+	for(int i=0;i<n;i++){
+	if(ngto(a[i])){
+	++cnt;
+	sum+=a[i];
+	}
+	}
+	printf("%.3lf",(double) sum/cnt);
+	return 0;
+	}
